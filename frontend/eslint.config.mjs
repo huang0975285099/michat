99c89383724766542d11b69cb53be6cc4ccb0da3
gongchen <gongchen@mzjy.com>
2026-08-04 20:50:12 -0700

    chore: add ESLint flat config

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,32 @@
+import vue from 'eslint-plugin-vue'
+
+export default [
+  {
+    ignores: ['dist/**', '.quasar/**', 'node_modules/**'],
+  },
+  ...vue.configs['flat/essential'],
+  {
+    files: ['**/*.{js,mjs,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        window: 'readonly', document: 'readonly', navigator: 'readonly',
+        localStorage: 'readonly', sessionStorage: 'readonly', URL: 'readonly',
+        URLSearchParams: 'readonly', WebSocket: 'readonly', setTimeout: 'readonly',
+        clearTimeout: 'readonly', setInterval: 'readonly', clearInterval: 'readonly',
+        console: 'readonly', process: 'readonly',
+      },
+    },
+    rules: {
+      'no-undef': 'error',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+      'vue/multi-word-component-names': 'off',
+      'vue/no-v-html': 'off',
+    },
+  },
+  {
+    files: ['**/*.vue'],
+    rules: { 'no-unused-vars': 'off' },
+  },
+]
